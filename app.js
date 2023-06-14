3285fc26d3c98251d4db58166d4a2a4f9957fd01
IBRAHIM <ibrahimbajepade555@gmail.comgit config --global user.name IBRAHIM>
2023-06-14 18:18:34 +0300

    using passport and cookies update

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const ejs = require("ejs");
-const bcrypt =require("bcrypt");
-const saltRounds = 10;
 const _ = require("lodash");
+const Schema = mongoose.Schema;
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-// for the static file like images and css files e.t.c.
-app.use(express.static("public"));
-const schema = mongoose.Schema;
+app.use(bodyParser.urlencoded({ extended: true })); // this will get input from webpage
+app.use(express.static("public")); // for the static file like images and css files e.t.c.
+
+
+//setup session
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+app.use(passport.initialize()); //initalize passport
+app.use(passport.session());   // use passport to deal with session
 
 // Connect to MongoDB database
 mongoose
@@ -28,22 +41,22 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-// Define Todo schema
-const userSchema = new schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+// Define schema 
+const userSchema = new Schema({
+  email: String,
+  password: String,
 });
 
-
+/*************Password encryption *****************/
+userSchema.plugin(passportLocalMongoose);
 
 // Create Todo model
 const User = mongoose.model("User", userSchema);
+
+// Here db-model used to create strtegy and serialize and deserialize 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -57,38 +70,54 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save().then((value) => {
-      if (value) {
-        res.render("secrets");
-      }
-    });
-  });
-  
-});
+app.get("/secrets", (req,res) => {
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+})
 
-app.post("/login", (req, res) => {
-  
+app.get("/logout", (req,res) =>{
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+})
+
+app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ email: username }).then((value, err) => {
-    if (value) {
-      bcrypt.compare(password, value.password, function (err, result) {
-        if (result === true){
-          res.render("secrets");
-        }
-      }); 
-      
-    } else {
-      console.log(err);
-    }
+  
+    User.register({ username: username }, password, (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    });
+});
+
+app.post("/login", (req, res) => {
+  const user = new User ({
+    username: req.body.username,
+    password: req.body.password
   });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req,res, () => {
+        res.redirect("/secrets");
+      })
+    }
+  })
 });
 
 app.get("/submit", (req, res) => {
